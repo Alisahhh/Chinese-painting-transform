52b1934b798b2c99235c6a06372d5791781dec85
Alisahhh <923431911@qq.com>
2019-01-23 20:38:52 +0800

    unfinish

---
+++ DealTask.py
@@ -0,0 +1,102 @@
+import sqlite3
+import time
+import os
+
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+# print "Opened database successfully"
+# c = conn.cursor()
+# c.execute('''CREATE TABLE Painting
+#        (ID INT PRIMARY KEY     NOT NULL,
+#        STATUS INT      NOT NULL,
+#        RECVPATH        CHAR(50),
+#        LOADPATH        CHAR(50));''')
+# print "Table created successfully"
+# conn.commit()
+# conn.close()
+
+val = 1
+
+cursor = c.execute("SELECT *  from Painting ")
+for row in cursor:
+       print "ID = ", row[0]
+       print "STATUS = ", row[1]
+       print "RECVPATH = ", row[2]
+       print "LOADPATH = ", row[3], "\n"
+cursor = c.execute("SELECT *  from Painting where STATUS = 0") 
+#os.
+# for row in cursor:
+#        print ""
+#        print "ID = ", row[0]
+#        print "STATUS = ", row[1]
+#        print "RECVPATH = ", row[2]
+#        print "LOADPATH = ", row[3], "\n"
+for row in cursor:
+       #execute&save
+       UPLOAD_FOLDER =' /home/alisa/Chinese-painting-transform-/'
+       name = UPLOAD_FOLDER + str(row[0])+"_new."+ "jpg"
+       #row[3]=name
+       tmp="python3 inference.py --model ink2real.pb --input "+row[2]+" --output "+name
+       c.execute("UPDATE Painting set STATUS = 1 where ID = "+str(row[0]))
+       os.system(tmp)
+       print "OK"
+       #change statue
+       tmp = "UPDATE Painting set LOADPATH ="+name+ " where ID = "+str(row[0])
+       print tmp
+       c.execute("UPDATE Painting set STATUS = 1 where ID = "+str(row[0]))
+       c.execute("UPDATE Painting set STATUS = 2 where ID="+str(row[0]))
+
+cursor = c.execute("SELECT *  from Painting ") 
+for row in cursor:
+       print "ID = ", row[0]
+       print "STATUS = ", row[1]
+       print "RECVPATH = ", row[2]
+       print "LOADPATH = ", row[3], "\n"
+cursor = c.execute("SELECT *  from Painting where STATUS = 0") 
+#os.
+# for row in cursor:
+#        print ""
+#        print "ID = ", row[0]
+#        print "STATUS = ", row[1]
+#        print "RECVPATH = ", row[2]
+#        print "LOADPATH = ", row[3], "\n"
+for row in cursor:
+       #execute&save
+       UPLOAD_FOLDER =' /mnt/c/Users/Alisa/Documents/My File/code/winter camp/load/'
+       name = UPLOAD_FOLDER + str(row[0])+"_new."+ "jpg"
+       #row[3]=name
+       tmp="python3 inference.py --model ink2real.pb --input "+row[2]+" --output "+name
+       c.execute("UPDATE Painting set STATUS = 1 where ID = "+str(row[0]))
+       os.system(tmp)
+       print "OK"
+       #change statue
+       tmp = "UPDATE Painting set LOADPATH ="+name+ " where ID = "+str(row[0])
+       print tmp
+       c.execute("UPDATE Painting set STATUS = 1 where ID = "+str(row[0]))
+       c.execute("UPDATE Painting set STATUS = 2 where ID="+str(row[0]))
+
+# while val == 1:
+#        #print "OK"
+#        cursor = c.execute("SELECT *  from Painting where STATUS = 0")
+#        #os.
+#        # for row in cursor:
+#        #        print ""
+#        #        print "ID = ", row[0]
+#        #        print "STATUS = ", row[1]
+#        #        print "RECVPATH = ", row[2]
+#        #        print "LOADPATH = ", row[3], "\n"
+#        for row in cursor:
+#               #execute&save
+#               UPLOAD_FOLDER =' /mnt/c/Users/Alisa/Documents/My File/code/winter camp/load/'
+#               name = UPLOAD_FOLDER + str(row[0])+"_new."+ "jpg"
+#               #row[3]=name
+#               tmp="python3 inference.py --model ink2real.pb --input "+row[2]+" --output "+name
+#               c.execute("UPDATE Painting set STATUS = 1 where ID = "+str(row[0]))
+#               os.system(tmp)
+#               print "OK"
+#               #change statue
+#               tmp = "UPDATE Painting set LOADPATH ="+name+ " where ID = "+str(row[0])
+#               print tmp
+#               c.execute("UPDATE Painting set STATUS = 1 where ID = "+str(row[0]))
+#               c.execute("UPDATE Painting set STATUS = 2 where ID="+str(row[0]))
+#        #time.sleep(5)
---
+++ index.py
@@ -0,0 +1,118 @@
+from flask import Flask
+import sqlite3
+from flask import request
+from werkzeug.utils import secure_filename
+import json
+from flask import render_template
+import base64
+app = Flask(__name__)
+
+PaintingCount = 0
+
+@app.route('/')
+@app.route('/api/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/task',methods=['GET'])
+def TaskList():
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    cursor = c.execute("SELECT ID, STATUS  from Painting")
+    ret = [] 
+    for i in cursor:
+        tmp = {'ID': i[0], 'STATUS': i[1]} 
+        ret.append(tmp)
+    ret = json.dumps(ret, indent=4)
+    return ret
+
+@app.route('/api/task/create',methods=['POST','GET'])
+def CreateTask():
+    #print "OK"
+    global PaintingCount
+    status = 0
+    PaintingID=-1
+    if request.method == 'POST':
+        # print "OK"
+        PaintingCount = PaintingCount+1
+        f = request.files['file']
+        # print "OK"
+        UPLOAD_FOLDER ="/home/alisa/Chinese-painting-transform-/"
+        print UPLOAD_FOLDER[0:]
+        name = UPLOAD_FOLDER + str(PaintingCount)+"."+ secure_filename(f.filename)
+
+        print name[0:]
+        f.save(name)
+        # print "OK"
+        PaintingID=PaintingCount
+        conn = sqlite3.connect('test.db')
+        c = conn.cursor()
+        tmp = "INSERT INTO Painting(ID ,STATUS,RECVPATH,LOADPATH ) VALUES("+str(PaintingID)+",0,'"+name+"','')" 
+        print tmp
+        c.execute(tmp)
+        conn.commit()
+        conn.close()
+    ret = [status,PaintingID]
+    # ret.append(status)
+    # ret.append(PaintingID)
+    ret = json.dumps(ret,indent=4)
+    return ret
+
+@app.route('/api/task/<id>/status',methods=['GET'])
+def GetTaskStatus(id):
+    status=0
+    if request.method == 'GET':
+        conn = sqlite3.connect('test.db')
+        c = conn.cursor()
+        cursor = c.execute("SELECT ID, STATUS  from Painting")
+        for row in cursor:
+            if id == row[0]:
+                status = 1
+                ret = [status,row[1]]
+                ret =  json.dumps(ret,indent=4)
+                return ret
+    ret = [status,-1]
+    ret = json.dumps(ret,indent=4)
+    return ret
+
+def return_img_stream(img_local_path):
+    img_stream = ''
+    print "OK"
+    with open(img_local_path, 'r') as img_f:
+        img_stream = img_f.read()
+        img_stream = base64.b64encode(img_stream)
+        print "ok"
+    return img_stream
+
+@app.route('/api/task/<id>/download',methods=['GET'])
+def DownloadPainting(id):
+    status=0
+    if request.method == 'GET':
+        conn = sqlite3.connect('test.db')
+        c = conn.cursor()
+        cursor = c.execute("SELECT ID, LOADPATH  from Painting")
+        for row in cursor:
+            # print type(row[0])
+            # print type(int(id))
+            # print id == row[0]
+            if int(id) == row[0]:
+                print row[1]
+                img_stream = return_img_stream(row[1])
+                return render_template('showpainting.html',img_stream=img_stream)
+    ret=[status]
+    ret = json.dumps(ret,indent=4)
+    return ret
+
+@app.route('/api/task/<id>',methods=['DELETE'])
+def DeletePainting(id):
+    status=0
+    if request.method == 'DELETE':    
+        conn = sqlite3.connect('test.db')
+        c = conn.cursor()
+        c.execute("DELETE from COMPANY where ID="+id)    
+        conn.commit()
+        conn.close()
+        status=1
+    ret=[1]
+    ret = json.dumps(ret,indent=4)
+    return ret
